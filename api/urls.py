36eaf376117a75b9f2a432d26bb9d0e58ac49787
djumanov <diyorbekjumanov1120@gmail.com>
2022-10-30 14:07:25 +0500

    get products view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
-from os import name
 from django.urls import path
 from .views import (
     CompaniesView,
+    ProductsView,
 )
 
 urlpatterns = [
     path(route='companies/', view=CompaniesView.as_view(), name='companies'),
+    path(route='products/', view=ProductsView.as_view(), name='products'),
 ]
--- api/views.py
+++ api/views.py
@@ -4,6 +4,26 @@ from .models import Company, Product
 import json
 
 
+def convert_to_dict(product: Product) -> dict:
+    company: Company = Company.objects.get(id=product.company_id)
+    product_dict = {
+        'id': product.id,
+        'name': product.name,
+        'color': product.color,
+        'ram': product.ram,
+        'memory': product.memory,
+        'price': product.price,
+        'image': product.image,
+        'released_date': product.released_date,
+        'created_at': product.created_at,
+        'updated_at': product.updated_at,
+        'company': company.name
+    }
+
+    return product_dict
+
+
+
 class CompaniesView(View):
     def get(self, request: HttpRequest) -> JsonResponse:
         companies = Company.objects.all()
@@ -17,3 +37,14 @@ class CompaniesView(View):
             }
 
         return JsonResponse(data=companies_json)
+
+
+class ProductsView(View):
+    def get(self, request: HttpRequest) -> JsonResponse:
+        products = Product.objects.all()
+        products_json = {'products': []}
+        for product in products:
+            products_json['products'].append(convert_to_dict(product))
+
+        return JsonResponse(data=products_json)
+
