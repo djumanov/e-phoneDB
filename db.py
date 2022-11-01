0bf3882714266896e34871147c5f5966166a0907
djumanov <diyorbekjumanov1120@gmail.com>
2022-11-01 09:48:03 +0500

    test views

---
+++ db.py
@@ -0,0 +1,47 @@
+import json
+from tinydb import TinyDB
+import requests
+import random
+from datetime import date
+
+base_url = 'http://127.0.0.1:8000/api/'
+
+rams = [3, 4, 6, 8, 12]
+memories = [16, 32, 64, 128, 256, 512]
+
+day = list(range(1, 28))
+month = list(range(1, 12))
+year = list(range(2010, 2022))
+
+db = TinyDB('db.json')
+collections = db.tables()
+# print(collections)
+
+# for collection_name in collections:
+#     payload = {
+#         'name': collection_name,
+#         'logo': collection_name,
+#         'description': collection_name + ' description',
+#         'website': f'www.{collection_name.lower()}.com'
+#     }
+#     r = requests.post(url=base_url+'add_company/', data=payload)
+#     print(r.status_code)
+
+
+for collection_name in collections:
+    table = db.table(name=collection_name)
+    documents = table.all()
+    for document in documents:
+        payload = {
+            'name': document['name'],
+            'color': document['color'],
+            'ram': random.choice(rams),
+            'memory': random.choice(memories),
+            'price': float(document['price']),
+            'image': document['img_url'],
+            'released_date': str(date(day=random.choice(day), month=random.choice(month), year=random.choice(year))),
+            'company': document['company']
+        }
+        # print(payload['released_date'])
+        r = requests.post(url=base_url+'create_product/', data=payload)
+        print(r.json())
